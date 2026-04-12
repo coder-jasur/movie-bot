6ab5ae54ceecd0a8e8f873834a3de75f45abfa87
Jasur <jasurhikmatilloyev08@gmail.com>
2026-04-12 09:34:59 +0500

    add all

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -25,6 +25,7 @@ class Settings:
     userbot_session: str | None = None
     telegram_api_id: int | None = None
     telegram_api_hash: str | None = None
+    telegram_local: bool = False
 
     def construct_postgresql_url(self):
         postgresql_dsn = (
@@ -57,5 +58,6 @@ def load_config() -> Settings:
         use_webhook=env.bool("USE_WEBHOOK", False),
         userbot_session=env.str("USERBOT_SESSION_STRING", None),
         telegram_api_id=env.int("TELEGRAM_API_ID", None),
-        telegram_api_hash=env.str("TELEGRAM_API_HASH", None)
+        telegram_api_hash=env.str("TELEGRAM_API_HASH", None),
+        telegram_local=env.bool("TELEGRAM_LOCAL", False)
     )
